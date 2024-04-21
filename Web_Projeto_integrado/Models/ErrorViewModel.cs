b49d144d0911cf74066ea99a06a3e03a5d8ba883
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2024-04-20 23:06:08 -0300

    trying to fix one bug

--- Web_Projeto_integrado/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Web_Projeto_integrado.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
