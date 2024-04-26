3d693159850252f88692480f82228347eb328204
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2024-04-26 20:43:34 -0300

    The site

---
+++ Web_Projeto_integrado/Controllers/EmprestimosController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_Projeto_integrado.Data;
+using Web_Projeto_integrado.Models;
+
+namespace Web_Projeto_integrado.Controllers
+{
+    public class EmprestimosController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+
+        public EmprestimosController(ApplicationDbContext db) 
+        { 
+          _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<EmprestimosModel> emprestimos = _db.Emprestimos;
+            return View(emprestimos);
+        }
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(EmprestimosModel emprestimos) 
+        { 
+          if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Add(emprestimos);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+          return View();
+        }
+        [HttpGet]
+        public IActionResult Editar(int? id) 
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+        [HttpGet]
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EmprestimosModel emprestimo = _db.Emprestimos.FirstOrDefault(x=>x.Id == id);
+
+            if(emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            return View(emprestimo);
+        }
+        [HttpPost]
+        public IActionResult Excluir(EmprestimosModel emprestimo)
+        {
+            if (emprestimo == null)
+            {
+                return NotFound();
+            }
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Editar( EmprestimosModel emprestimo)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Emprestimos.Update(emprestimo);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emprestimo);
+        }
+    }
+}
---
+++ Web_Projeto_integrado/Models/EmprestimosModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Projeto_integrado.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Digite o nome do Recebedor!")]
+        public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
+        public string fornecedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
+        public string LivroEmprestado { get; set; }
+
+        public DateTime Dataultimaatualizacao { get; set; } = DateTime.Now;
+
+    }
+}
